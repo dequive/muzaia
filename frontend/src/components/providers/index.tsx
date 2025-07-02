001fe1e1839d32dd1c15d60fa911b27b8c0c29bc
Delson Quive <dequive@gmail.com>
2025-07-02 04:27:48 +0200

    Update index.tsx

--- frontend/src/components/providers/index.tsx
+++ frontend/src/components/providers/index.tsx
@@ -1,16 +1,28 @@
 'use client'
 
 import { ThemeProvider } from 'next-themes'
-import { ReactNode } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
+
+const queryClient = new QueryClient()
 
 interface ProvidersProps {
-  children: ReactNode
+  children: React.ReactNode
 }
 
 export function Providers({ children }: ProvidersProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      {children}
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider 
+        attribute="class" 
+        defaultTheme="system" 
+        enableSystem
+      >
+        {children}
+        <Toaster position="top-right" expand richColors closeButton />
+      </ThemeProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
